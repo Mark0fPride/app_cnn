a49816b20bb5c8468b43e18d92e68dccfb0ef5b0
Marek N <02nmarek@gmail.com>
2025-10-19 17:48:07 +0200

    init

---
+++ app/src/main/java/com/cnn/mushroom/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.cnn.mushroom
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.cnn.mushroom.ui.theme.CNNTheme
+import com.cnn.mushroom.ui.screens.Greeting
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CNNTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
